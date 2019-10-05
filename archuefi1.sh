34abb79d0009d75a4daf41b6418275ba0afe563e
ordanax <ordanax@ya.ru>
2019-10-06 01:58:30 +0500

    Update archuefi1.sh

--- archuefi1.sh
+++ archuefi1.sh
@@ -42,15 +42,16 @@ echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.fat -F32 /dev/sda1 
-mkfs.ext4  /dev/sda2
+
+mkfs.ext4  /dev/sda1
+mkfs.fat -F32 /dev/sda2
 mkfs.ext4  /dev/sda3
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sda2 /mnt
+mount /dev/sda1 /mnt
 mkdir /mnt/home
 mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mount /dev/sda2 /mnt/boot/efi
 mount /dev/sda3 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
