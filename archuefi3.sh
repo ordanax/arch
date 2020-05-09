2f7d978c43d6dbda5b91aadb6308d1c48f964aab
ordanax <ordanax@ya.ru>
2020-05-09 13:50:18 +0500

    chromium

--- archuefi3.sh
+++ archuefi3.sh
@@ -19,7 +19,7 @@ echo 'Установить рекомендумые программы?'
 read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
   #Можно заменить на pacman -Qqm > ~/.pacmanlist.txt
-  sudo pacman -S recoll obs-studio veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru kdenlive neofetch vlc qbittorrent galculator telegram-desktop --noconfirm
+  sudo pacman -S recoll chromium obs-studio veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru kdenlive neofetch vlc qbittorrent galculator telegram-desktop --noconfirm
   wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.aurlist.txt
   yay -S --noconfirm - < .aurlist.txt
 elif [[ $prog_set == 0 ]]; then
