0b03be456471db0502a1d54cf300a261384f4dc0
ordanax <ordanax@ya.ru>
2020-05-28 19:54:16 +0500

    Авто вход без DE

--- archuefi3.sh
+++ archuefi3.sh
@@ -71,30 +71,27 @@ fi
 #  echo 'Пропускаем.'
 #fi
 
-#echo 'Убираем меню граб для выбора системы?'
-#read -p "1 - Да, 0 - Нет: " grub_set
-#if [[ $grub_set == 1 ]]; then
-#  wget git.io/grub.tar.gz
-#  sudo tar -xzf grub.tar.gz -C /
-#  sudo grub-mkconfig -o /boot/grub/grub.cfg
-#elif [[ $grub_set == 0 ]]; then
-#  echo 'Пропускаем.'
-#fi
-
-#echo 'Убираем DE?'
-#read -p "1 - Да, 0 - Нет: " node_set
-#if [[ $node_set == 1 ]]; then
-#sudo pacman -S xorg-xinit
-#cp /etc/X11/xinit/xserverrc ~/.xserverrc
-#wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.xinitrc
-#sudo rm -rf ~/.bashrc
-#wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.bashrc
-#su -c 'read -p "Введите ваш логин: " nodelogin && echo "[Service]" > /etc/systemd/system/getty@tty1.service.d/override.conf && echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/override.conf && echo "ExecStart=-/usr/bin/agetty --autologin $nodelogin --noclear %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/override.conf'
-#sudo systemctl disable lxdm
-#sudo pacman -R lxdm
-#elif [[ $node_set == 0 ]]; then
-#  echo 'Пропускаем.'
-#fi
+echo 'Делаем авто вход без DE?'
+read -p "1 - Да, 0 - Нет: " node_set
+if [[ $node_set == 1 ]]; then
+sudo systemctl disable lxdm
+sudo pacman -R lxdm
+sudo pacman -S xorg-xinit
+cp /etc/X11/xinit/xserverrc ~/.xserverrc
+wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.xinitrc
+sudo mv -f .xinitrc ~/.xinitrc
+wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.bashrc
+rm ~/.bashrc
+sudo mv -f .bashrc ~/.bashrc
+wget https://raw.githubusercontent.com/ordanax/arch/master/attach/grub
+sudo mv -f grub /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+read -p "Введите имя пользователя: " username
+echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $username --noclear %l $TERM" > override.conf
+sudo mv -f override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
+elif [[ $node_set == 0 ]]; then
+  echo 'Пропускаем.'
+fi
 
 echo 'Установить conky?'
 read -p "1 - Да, 0 - Нет: " conky_set
