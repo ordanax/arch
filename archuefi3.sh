fe96b0a15a4c71d1cfa8aae360f5bbbef0afaea9
ordanax <ordanax@ya.ru>
2020-06-02 16:07:55 +0500

    Update archuefi3.sh

--- archuefi3.sh
+++ archuefi3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf ~/.config/xfce4/*
 mkdir ~/downloads
 cd ~/downloads
 
@@ -37,8 +36,7 @@ if [[ $xfce_set == 1 ]]; then
   # tar -czf config.tar.gz .config
   # Выгрузите архив в интернет и скорректируйте ссылку на свою.
   wget https://github.com/ordanax/arch/raw/master/attach/config.tar.gz
-  sudo rm -rf ~/.config/xfce4/panel/
-  sudo rm -rf ~/.config/xfce4/*
+  sudo rm -rf ~/.config/xfce4
   sudo tar -xzf config.tar.gz -C ~/
   echo 'Удаление тем по умолчанию'
   sudo rm -rf /usr/share/themes/*
