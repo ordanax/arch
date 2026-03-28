c7022ec35eb57128424816376689fd312b0c527e
ordanax <ordanax@ya.ru>
2026-03-28 17:18:21 +0300

    Add files via upload

--- archuefi3.sh
+++ archuefi3.sh
@@ -1,120 +1,333 @@
 #!/bin/bash
-mkdir ~/downloads
+
+# =============================================================================
+# Arch Linux Fast Install v3.0.0 - Часть 3: Настройка и программы
+# Запускать ПОСЛЕ первой перезагрузки и входа в систему
+# =============================================================================
+
+set -e
+
+echo "╔════════════════════════════════════════════════════════════════════╗"
+echo "║     Arch Linux Fast Install v3.0.0 - Настройка системы             ║"
+echo "║     Установка программ и конфигурация                             ║"
+echo "╚════════════════════════════════════════════════════════════════════╝"
+echo ""
+
+mkdir -p ~/downloads
 cd ~/downloads
 
-echo 'Установка AUR (yay)'
-sudo pacman -Syu
-sudo pacman -S wget --noconfirm
-wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
+# =============================================================================
+# 1. УСТАНОВКА YAY (AUR helper)
+# =============================================================================
 
-echo 'Создаем нужные директории'
-sudo pacman -S xdg-user-dirs --noconfirm
+echo "📦 Установка AUR helper (yay)..."
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm git base-devel
+
+# Установка yay
+if ! command -v yay &> /dev/null; then
+    echo "⬇️  Скачивание и установка yay..."
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    cd ~/downloads
+    rm -rf /tmp/yay
+    echo "✅ Yay установлен!"
+else
+    echo "✅ Yay уже установлен"
+fi
+
+# =============================================================================
+# 2. СОЗДАНИЕ ДИРЕКТОРИЙ
+# =============================================================================
+
+echo "📁 Создание стандартных директорий..."
+sudo pacman -S --noconfirm xdg-user-dirs
 xdg-user-dirs-update
 
-echo 'Установка базовых программ и пакетов'
-sudo pacman -S reflector firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol --noconfirm
+# =============================================================================
+# 3. БАЗОВЫЕ ПРОГРАММЫ
+# =============================================================================
 
-echo 'Установить рекомендумые программы?'
+echo "📦 Установка базовых программ..."
+sudo pacman -S --noconfirm \
+    reflector \
+    firefox firefox-i18n-ru \
+    ufw gufw \
+    f2fs-tools dosfstools ntfs-3g \
+    file-roller p7zip unrar \
+    gvfs \
+    aspell-ru \
+    man-db man-pages texinfo
+
+# =============================================================================
+# 4. РЕКОМЕНДУЕМЫЕ ПРОГРАММЫ
+# =============================================================================
+
+echo ""
+echo "Установить рекомендуемые программы?"
+echo "  (LibreOffice, GIMP, VLC, Telegram, Discord, Obsidian и др.)"
 read -p "1 - Да, 0 - Нет: " prog_set
-if [[ $prog_set == 1 ]]; then
-  #Можно заменить на pacman -Qqm > ~/.pacmanlist.txt
-  sudo pacman -S recoll chromium flameshot obs-studio veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru kdenlive neofetch qbittorrent galculator telegram-desktop viewnior --noconfirm
-  yay -Syy
-  yay -S xflux sublime-text-dev hunspell-ru pamac-aur-git megasync-nopdfium trello xorg-xkill ttf-symbola ttf-clear-sans --noconfirm
-elif [[ $prog_set == 0 ]]; then
-  echo 'Установка программ пропущена.'
+
+if [[ $prog_set == "1" ]]; then
+    echo "⬇️  Установка программ из официальных репозиториев..."
+    sudo pacman -S --noconfirm \
+        libreoffice-fresh libreoffice-fresh-ru \
+        gimp inkscape \
+        vlc mpv ffmpeg \
+        thunderbird \
+        telegram-desktop \
+        qbittorrent \
+        filezilla \
+        obs-studio \
+        veracrypt \
+        flameshot \
+        neofetch btop htop \
+        galculator \
+        viewnior \
+        pcmanfm \
+        gparted \
+        ark
+    
+    echo "⬇️  Установка программ из AUR..."
+    yay -S --noconfirm \
+        sublime-text-4 \
+        hunspell-ru \
+        papirus-icon-theme \
+        capitaine-cursors \
+        ttf-clear-sans
+        # Удалены: xflux (сервис закрыт), megasync (проблемы со сборкой)
+    
+    echo "✅ Программы установлены!"
+else
+    echo "⏭️  Установка программ пропущена"
 fi
 
-echo 'Скачать и установить конфиг и темы для XFCE?'
+# =============================================================================
+# 5. КОНФИГУРАЦИЯ XFCE
+# =============================================================================
+
+echo ""
+echo "Скачать и установить конфиг и темы для XFCE?"
 read -p "1 - Да, 0 - Нет: " xfce_set
-if [[ $xfce_set == 1 ]]; then
-  echo 'Качаем и устанавливаем настройки Xfce'
-  # Чтобы сделать копию ваших настоек перейдите в домашнюю директорию ~/username 
-  # открйте в этой категории терминал и выполните команду ниже
-  # Предварительно можно очистить конфиг от всего лишнего
-  # tar -czf config.tar.gz .config
-  # Выгрузите архив в интернет и скорректируйте ссылку на свою.
-  wget https://github.com/ordanax/arch/raw/master/attach/config.tar.gz
-  sudo rm -rf ~/.config/xfce4/*
-  sudo tar -xzf config.tar.gz -C ~/
-  echo 'Удаление тем по умолчанию'
-  sudo rm -rf /usr/share/themes/*
-  echo 'Установка тем'
-  yay -S x-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme --noconfirm
-  sudo pacman -S capitaine-cursors --noconfirm
-  
-  echo 'Ставим лого ArchLinux в меню'
-  wget git.io/arch_logo.png
-  sudo mv -f ~/downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
-  
-  echo 'Удаляем лишнее из xfce4'
-  sudo pacman -Rs xfburn orage parole mousepad xfce4-appfinder xfce4-clipman-plugin xfce4-timer-plugin xfce4-time-out-plugin xfce4-artwork xfce4-taskmanager xfce4-smartbookmark-plugin xfce4-sensors-plugin xfce4-screenshooter xfce4-notes-plugin xfce4-netload-plugin xfce4-mpc-plugin xfce4-mount-plugin xfce4-mailwatch-plugin xfce4-genmon-plugin xfce4-fsguard-plugin xfce4-eyes-plugin xfce4-diskperf-plugin xfce4-dict xfce4-cpugraph-plugin xfce4-cpufreq-plugin
 
-  echo 'Ставим обои на рабочий стол'
-  wget git.io/bg.jpg
-  sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартные обои
-  sudo mv -f ~/downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
-elif [[ $xfce_set == 0 ]]; then
-  echo 'Установка конфигов XFCE пропущена.'
-fi 
-
-echo "Ставим i3 с моими настройками?"
-read -p "1 - Да, 2 - Нет: " vm_setting
-if [[ $vm_setting == 1 ]]; then
-    pacman -S i3-wm dmenu pcmanfm ttf-font-awesome feh gvfs udiskie xorg-xbacklight ristretto tumbler compton jq --noconfirm
-    yay -S polybar ttf-weather-icons ttf-clear-sans
-    wget https://github.com/ordanax/arch/raw/master/attach/config_i3wm.tar.gz
-    sudo rm -rf ~/.config/i3/*
-    sudo rm -rf ~/.config/polybar/*
-    sudo tar -xzf config_i3wm.tar.gz -C ~/
-elif [[ $vm_setting == 2 ]]; then
-  echo 'Пропускаем.'
+if [[ $xfce_set == "1" ]]; then
+    echo "🎨 Установка тем и конфигов XFCE..."
+    
+    # Темы из AUR
+    yay -S --noconfirm arc-gtk-theme papirus-maia-icon-theme-git
+    sudo pacman -S --noconfirm capitaine-cursors
+    
+    # Скачивание конфига (если URL актуален)
+    echo "⬇️  Скачивание конфигурации XFCE..."
+    if wget -q --spider https://github.com/ordanax/arch/raw/master/attach/config.tar.gz 2>/dev/null; then
+        wget -q https://github.com/ordanax/arch/raw/master/attach/config.tar.gz
+        sudo rm -rf ~/.config/xfce4/*
+        tar -xzf config.tar.gz -C ~/
+        echo "✅ Конфиг XFCE установлен"
+    else
+        echo "⚠️  URL конфига недоступен, пропускаем"
+    fi
+    
+    # Лого Arch
+    if wget -q --spider https://raw.githubusercontent.com/ordanax/arch/master/attach/arch_logo.png 2>/dev/null; then
+        wget -q https://raw.githubusercontent.com/ordanax/arch/master/attach/arch_logo.png
+        sudo mv -f arch_logo.png /usr/share/pixmaps/arch_logo.png
+    fi
+    
+    # Удаление лишних пакетов XFCE (опционально)
+    echo ""
+    echo "Удалить неиспользуемые плагины XFCE?"
+    read -p "1 - Да, 0 - Нет: " remove_xfce
+    if [[ $remove_xfce == "1" ]]; then
+        sudo pacman -Rs --noconfirm \
+            xfburn orage parole mousepad \
+            xfce4-appfinder xfce4-clipman-plugin \
+            xfce4-timer-plugin xfce4-time-out-plugin \
+            xfce4-artwork xfce4-taskmanager \
+            2>/dev/null || true
+    fi
+    
+    # Обои
+    if wget -q --spider https://raw.githubusercontent.com/ordanax/arch/master/attach/bg.jpg 2>/dev/null; then
+        wget -q https://raw.githubusercontent.com/ordanax/arch/master/attach/bg.jpg
+        sudo rm -rf /usr/share/backgrounds/xfce/*
+        sudo mv -f bg.jpg /usr/share/backgrounds/xfce/bg.jpg
+    fi
+    
+    echo "✅ Настройка XFCE завершена!"
+else
+    echo "⏭️  Установка конфигов XFCE пропущена"
 fi
 
-echo 'Установить conky?'
+# =============================================================================
+# 6. I3WM (опционально)
+# =============================================================================
+
+echo ""
+echo "Установить i3wm с настройками?"
+read -p "1 - Да, 0 - Нет: " i3_choice
+
+if [[ $i3_choice == "1" ]]; then
+    echo "🪟 Установка i3wm..."
+    sudo pacman -S --noconfirm \
+        i3-wm i3status dmenu \
+        pcmanfm \
+        ttf-font-awesome \
+        feh \
+        udiskie \
+        xorg-xbacklight \
+        ristretto \
+        tumbler \
+        picom  # compton заменен на picom
+    
+    yay -S --noconfirm polybar ttf-weather-icons
+    
+    # Скачивание конфига i3
+    if wget -q --spider https://github.com/ordanax/arch/raw/master/attach/config_i3wm.tar.gz 2>/dev/null; then
+        wget -q https://github.com/ordanax/arch/raw/master/attach/config_i3wm.tar.gz
+        rm -rf ~/.config/i3/* ~/.config/polybar/*
+        tar -xzf config_i3wm.tar.gz -C ~/
+        echo "✅ Конфиг i3wm установлен"
+    else
+        echo "⚠️  URL конфига i3 недоступен"
+    fi
+else
+    echo "⏭️  Установка i3wm пропущена"
+fi
+
+# =============================================================================
+# 7. CONKY
+# =============================================================================
+
+echo ""
+echo "Установить Conky?"
 read -p "1 - Да, 0 - Нет: " conky_set
-if [[ $conky_set == 1 ]]; then
-  sudo pacman -S conky conky-manager --noconfirm
-  wget git.io/conky.tar.gz
-  tar -xzf conky.tar.gz -C ~/
-elif [[ $conky_set == 0 ]]; then
-  echo 'Установка conky пропущена.'
+
+if [[ $conky_set == "1" ]]; then
+    sudo pacman -S --noconfirm conky conky-manager
+    
+    if wget -q --spider https://raw.githubusercontent.com/ordanax/arch/master/attach/conky.tar.gz 2>/dev/null; then
+        wget -q https://raw.githubusercontent.com/ordanax/arch/master/attach/conky.tar.gz
+        tar -xzf conky.tar.gz -C ~/
+    fi
 fi
 
-echo 'Делаем авто вход без DE?'
-read -p "1 - Да, 0 - Нет: " node_set
-if [[ $node_set == 1 ]]; then
-sudo systemctl disable lxdm
-sudo pacman -R lxdm
-sudo pacman -S xorg-xinit --noconfirm
-cp /etc/X11/xinit/xserverrc ~/.xserverrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.xinitrc
-sudo mv -f .xinitrc ~/.xinitrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.bashrc
-rm ~/.bashrc
-sudo mv -f .bashrc ~/.bashrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/grub
-sudo mv -f grub /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-read -p "Введите имя пользователя: " username
-sudo echo -e '[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin' "$username" '--noclear %I $TERM' > ~/downloads/override.conf
-sudo mkdir /etc/systemd/system/getty@tty1.service.d/
-sudo mv -f ~/downloads/override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
-elif [[ $node_set == 0 ]]; then
-  echo 'Пропускаем.'
+# =============================================================================
+# 8. ZRAM
+# =============================================================================
+
+echo ""
+echo "Включить zram (сжатие RAM)?"
+read -p "1 - Да, 0 - Нет: " zram_choice
+
+if [[ $zram_choice == "1" ]]; then
+    yay -S --noconfirm zram-generator
+    sudo systemctl enable systemd-zram-setup@zram0.service
 fi
 
-# Подключаем zRam
-yay -S zramswap --noconfirm
-sudo systemctl enable zramswap.service
+# =============================================================================
+# 9. ФАЙРВОЛ
+# =============================================================================
 
-echo 'Включаем сетевой экран'
+echo "🔥 Включение файрвола UFW..."
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
 sudo ufw enable
-
-echo 'Добавляем в автозагрузку:'
 sudo systemctl enable ufw
 
-# Очистка
-rm -rf ~/downloads/
+# =============================================================================
+# 10. АВТОВХОД БЕЗ DM (опционально)
+# =============================================================================
 
-echo 'Установка завершена!'
+echo ""
+echo "Настроить автовход без менеджера входа (LightDM)?"
+echo "⚠️  Только если вы знаете что делаете!"
+read -p "1 - Да, 0 - Нет: " node_set
+
+if [[ $node_set == "1" ]]; then
+    echo "🔧 Настройка автовхода..."
+    
+    # Отключаем LightDM
+    sudo systemctl disable lightdm
+    sudo pacman -R --noconfirm lightdm lightdm-gtk-greeter
+    
+    # Устанавливаем xinit
+    sudo pacman -S --noconfirm xorg-xinit
+    
+    # Создаем .xinitrc
+    cat > ~/.xinitrc << 'XEOF'
+#!/bin/sh
+# Xfce по умолчанию
+session=${1:-xfce}
+
+case $session in
+    xfce|xfce4) exec startxfce4 ;;
+    i3|i3wm) exec i3 ;;
+    *) exec $1 ;;
+esac
+XEOF
+    chmod +x ~/.xinitrc
+    
+    # Настраиваем автовход
+    sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+    sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null << EOF
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I \$TERM
+EOF
+    
+    # Добавляем автозапуск X в .bash_profile
+    if ! grep -q "exec startx" ~/.bash_profile 2>/dev/null; then
+        echo '
+# Автозапуск X при входе на tty1
+if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+    exec startx
+fi' >> ~/.bash_profile
+    fi
+    
+    echo "✅ Автовход настроен!"
+    echo "⚠️  Перезагрузитесь для применения изменений"
+fi
+
+# =============================================================================
+# 11. ОЧИСТКА
+# =============================================================================
+
+echo "🧹 Очистка временных файлов..."
+cd ~
+rm -rf ~/downloads
+
+# =============================================================================
+# ЗАВЕРШЕНИЕ
+# =============================================================================
+
+echo ""
+echo "╔════════════════════════════════════════════════════════════════════╗"
+echo "║     ✅ УСТАНОВКА ЗАВЕРШЕНА!                                        ║"
+echo "╚════════════════════════════════════════════════════════════════════╝"
+echo ""
+echo "Установлено:"
+echo "  • Базовые программы"
+if [[ $prog_set == "1" ]]; then
+    echo "  • Офис, мультимедиа, интернет-программы"
+fi
+if [[ $xfce_set == "1" ]]; then
+    echo "  • Темы и конфиги XFCE"
+fi
+if [[ $i3_choice == "1" ]]; then
+    echo "  • i3wm"
+fi
+if [[ $conky_set == "1" ]]; then
+    echo "  • Conky"
+fi
+echo "  • Файрвол UFW"
+echo ""
+echo "Полезные команды:"
+echo "  yay -Syu           # Обновление системы"
+echo "  reflector ...      # Обновление зеркал"
+echo "  sudo ufw status    # Статус файрвола"
+echo ""
+echo "Группа Arch Linux: https://vk.com/arch4u"
+echo "Telegram: https://t.me/linux4at"
+echo ""
